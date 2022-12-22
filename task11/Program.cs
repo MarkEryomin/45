db1a395744bd84596fbaf34625a2c7a74f6d5f50
MarkEryomin <erjomin.mark@gmail.com>
2022-12-22 11:57:02 +0300

    task 11

---
+++ task11/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ( a%7==0 & a%23==0)
+{
+Console.WriteLine($"число {a} , кратно 7 и 23 !!!");
+}
+else Console.WriteLine($"число {a} , НЕ кратно 7 и 23 ");
