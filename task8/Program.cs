7a5f6750b8af804ee95c459bd3ddb4777fe48d65
MarkEryomin <erjomin.mark@gmail.com>
2022-12-22 09:58:54 +0300

    task 8

---
+++ task8/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int number = new Random().Next(10,100);
+Console.WriteLine(" Ваше случайное число : " +number);
+int digit1= number/10;
+int digit2= number%10;
+if (digit1==digit2)
+{
+  Console.WriteLine($"Первая цифра {digit1}, равна второй {digit2}");  
+}
+if (digit1>digit2)
+{
+    Console.WriteLine($"Первая цифра {digit1}, больше второй");
+
+}
+else Console.WriteLine($"Вторая цифра {digit2}, больше первой");
