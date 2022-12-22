ad36bfcd382780c54cbe2cb785433890db93f398
MarkEryomin <erjomin.mark@gmail.com>
2022-12-22 14:33:35 +0300

    home 5

---
+++ homework5/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход
+//  трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите какой использовать вариант");
+
+int number = new Random().Next(100,1000);
+Console.WriteLine(" Ваше случайное число : " +number);
+
+int Var = Convert.ToInt32(Console.ReadLine());
+int num2 =0;
+switch (Var)
+{
+    case 1:
+       
+        int num23=number%100;
+        num2=num23/10;
+        Console.WriteLine("Второе число : "+num2);
+        break;
+
+    case 2:
+    
+        int num12=number/10;
+        num2=num12%10;
+        Console.WriteLine("Второе число : "+num2);
+        break;
+
+
+}
+
