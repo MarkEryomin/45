fbf7f5e0dbee6a760efbcc7d1f9335d31330c7d6
MarkEryomin <erjomin.mark@gmail.com>
2022-12-22 10:03:43 +0300

    task 9

---
+++ task9/Program.cs
@@ -0,0 +1,6 @@
+﻿
+int number = new Random().Next(100,1000);
+Console.WriteLine(" Ваше случайное число : " +number);
+int digit1= number/100;
+int digit2= number%10;
+Console.WriteLine("Новое число : "+digit1+digit2);
