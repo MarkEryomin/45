3f7f3a0c424ec682d20124ce7ef3f9568007f899
MarkEryomin <erjomin.mark@gmail.com>
2022-12-22 10:21:50 +0300

    task 10

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+if (a%b==0)
+{
+ Console.WriteLine($"Число {a} , кратное {b}");   
+}
+else Console.WriteLine("Не кратно, остаток : "+a%b);
+}
+else Console.WriteLine("Ошибка: второе число больше первого");
