8f5cf9124907b2d9e821530096cd0320710292a3
Mark <erjomin.mark@gmail.com>
2023-01-17 20:28:10 +0300

    4st1

--- seminar4/newtask1/Program.cs
+++ seminar4/newtask1/Program.cs
@@ -1,4 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Напишите программу,которая принимает на вход число(А) и выдаёт сумму
+// всех чисел от 1 до А.
+// 7--28
+// 4--10
+// 8--36
 
+int Sum(int number)
+{
+int sum = 0;
+for(int i = 1; i <= number; i++)
+{
+sum+=i;
+}
+return sum;
+}
 
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = Sum(A);
+Console.WriteLine(sum);
+
+// 
