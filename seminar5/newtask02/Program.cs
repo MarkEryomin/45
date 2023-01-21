351822bc42dc7e6cd0b93f006cebb9d11e94b166
Mark <erjomin.mark@gmail.com>
2023-01-22 00:53:10 +0300

    end 5 seminar

---
+++ seminar5/newtask02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените
+// на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+int[] FillArrayWithRandom(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    return array;
+}
+
+int[] array = new int[5];
+FillArrayWithRandom(array);
+Console.WriteLine("Ваш случайный массив: ");
+Console.WriteLine(string.Join(", ", array));
+
+for(int i =0; i < array.Length; i++)
+{
+array[i]*=-1;
+}
+Console.WriteLine(string.Join(", ", array));
---
+++ seminar5/newtask03/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая 
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] FillArrayWithRandom(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(-9, 10);
+}
+return array;
+}
+
+int[] array = new int [5];
+FillArrayWithRandom(array);
+Console.Write("Ваш случайный массив: ");
+Console.Write(string.Join(", ", array));
+Console.WriteLine();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool isOk = false;
+for(int i = 0; i < array.Length; i++)
+{
+if(number == array[i])
+{
+isOk = true;
+break;
+}
+}
+if(isOk)
+{
+Console.WriteLine("Элемент найден");
+}
+else
+{
+Console.WriteLine("Элемент не найден");
+}
+
+
