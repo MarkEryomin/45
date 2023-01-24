10e99fadf86078424e9058fb94c7a693f8ddf30e
Mark <erjomin.mark@gmail.com>
2023-01-24 20:09:20 +0300

    end 6 seminar

---
+++ seminar6/task40/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 40: Напишите программу, которая принимает на
+// вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+void Treug(int A, int B, int C)
+{
+if(A < B + C && B < A + C && C < A + B)
+{
+Console.WriteLine("Треугольник существует");
+}
+else
+{
+Console.WriteLine("Треугольник не существует");
+}
+}
+
+Console.WriteLine("Введите длину первой стороны");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину второй стороны");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину третьей стороны");
+int C = Convert.ToInt32(Console.ReadLine());
+
+Treug(A, B, C);
